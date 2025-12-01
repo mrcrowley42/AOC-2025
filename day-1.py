a43c4a146cb0980513bf5331d6566e06957f959d
mrcrowley42 <mattcrowley42@me.com>
2025-12-01 16:34:05 +1100

    1

--- day-1.py
+++ day-1.py
@@ -1,4 +1,13 @@
 with open("example_inputs/day-1.txt", "rt") as file:
     data = file.read().splitlines()
 
-print(data)
+position = 50
+count = 0
+for line in data:
+    direction = 1 if line[0] == "R" else -1
+    amount = int(line[1:])
+    position = (position + (direction * amount)) % 99
+    if position == 0:
+        count += 1
+
+print(count)
