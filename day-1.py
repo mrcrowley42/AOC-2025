36bc19ab7f0212d5e362c8de8cbb072de2ad90a3
mrcrowley42 <mattcrowley42@me.com>
2025-12-01 21:09:56 +1100

    Update day-1.py

--- day-1.py
+++ day-1.py
@@ -1,13 +1,15 @@
-with open("example_inputs/day-1.txt", "rt") as file:
-    data = file.read().splitlines()
+with open("inputs/day-1.txt", "rt") as file:
+    data = [((1 if line[0] == "R" else -1) , int(line[1:])) for line in file.read().splitlines()]
 
+total_1, total_2 = 0, 0
 position = 50
-count = 0
-for line in data:
-    direction = 1 if line[0] == "R" else -1
-    amount = int(line[1:])
-    position = (position + (direction * amount)) % 99
+for delta, turns in data:
+    for _ in range(turns):
+        position = (position + delta) % 100
+        if position == 0:
+            total_2 += 1
     if position == 0:
-        count += 1
+        total_1 += 1
 
-print(count)
+print(total_1)
+print(total_2)
