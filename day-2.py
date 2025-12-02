763568f6eb7488add041ab1b59b8e187e668cddf
mrcrowley42 <mattcrowley42@me.com>
2025-12-02 19:51:03 +1100

    early exit

--- day-2.py
+++ day-2.py
@@ -1,26 +1,33 @@
-from time import time
-def main():
-    s = time()
-    with open("inputs/day-2.txt") as file:
-        ranges = [list(map(int, r.split("-"))) for r in file.read().split(",")]
+with open("inputs/day-2.txt") as file:
+    ranges = [list(map(int, r.split("-"))) for r in file.read().split(",")]
 
-    total_1 = 0
-    total_2 = 0
-    for start, end in ranges:
-        for num in range(start, end + 1):
-            str_num = str(num)
-            half = len(str_num) // 2
-            if str_num[0:half] == str_num[half:]:
-                total_1 += num
-            for j in range(1, half + 1):
-                if len(str_num) % j != 0:
-                    continue
-                if len({str_num[i:i+j] for i in range(0, len(str_num) + 1, j) if len(str_num[i:i+j])}) == 1:
-                    total_2 += num
-                    break
-                
-    print(time() - s)
-    print(total_1)
-    print(total_2)
+def is_invalid(str_num, j):
+    last = None
+    for i in range(0, len(str_num) + 1, j):
+        sub_str = str_num[i:i+j]
+        if not sub_str:
+            continue
+        if not last:
+            last = sub_str
+            continue
+        if sub_str != last:
+            return False
+    return True
 
-main()
+
+total_1, total_2 = 0, 0
+for start, end in ranges:
+    for num in range(start, end + 1):
+        str_num = str(num)
+        len_num = len(str_num)
+        half = len(str_num) // 2
+        if str_num[0:half] == str_num[half:]:
+            total_1 += num
+        for j in range(1, half + 1):
+            if len_num % j != 0:
+                continue      
+            if is_invalid(str_num, j):
+                total_2 += num
+                break
+print(total_1)
+print(total_2)
