0efac3e9232c077a506840399c5f06c791bd20f3
mrcrowley42 <mattcrowley42@me.com>
2025-12-02 17:35:16 +1100

    2

--- day-2.py
+++ day-2.py
@@ -1,15 +1,26 @@
-with open("day-2.txt") as file:
-    ranges = [line.split(",") for line in file.read().splitlines()]
+from time import time
+def main():
+    s = time()
+    with open("inputs/day-2.txt") as file:
+        ranges = [list(map(int, r.split("-"))) for r in file.read().split(",")]
 
-ranges = [list(map(int, r.split("-"))) for r in ranges]
+    total_1 = 0
+    total_2 = 0
+    for start, end in ranges:
+        for num in range(start, end + 1):
+            str_num = str(num)
+            half = len(str_num) // 2
+            if str_num[0:half] == str_num[half:]:
+                total_1 += num
+            for j in range(1, half + 1):
+                if len(str_num) % j != 0:
+                    continue
+                if len({str_num[i:i+j] for i in range(0, len(str_num) + 1, j) if len(str_num[i:i+j])}) == 1:
+                    total_2 += num
+                    break
+                
+    print(time() - s)
+    print(total_1)
+    print(total_2)
 
-total = 0
-
-for start, end in ranges:
-    for num in range(start, end + 1):
-        str_num = str(num)
-        half = len(str_num) // 2
-        if str_num[0:half] == str_num[half:]:
-            total += num
-
-print(num)
+main()
