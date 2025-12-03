b964975a5f92713520346d084a46a6e856a86b4c
mrcrowley42 <mattcrowley42@me.com>
2025-12-03 17:39:55 +1100

    3.1

--- day-3.py
+++ day-3.py
@@ -1,19 +1,23 @@
+from time import time
+s = time()
 with open("inputs/day-3.txt") as file:
     data = [list(map(int, list(line))) for line in file.read().splitlines()]
 
 
 def get_total_jolts(n):
+    """Check for the first highest number, keeping enough room to end up with ``n`` batteries."""
     total = 0
     for bank in data:
-        answer = []
+        batteries = []
         for i in range((-1 * n) + 1, 0, 1):
             highest = max(bank[:i])
-            answer.append(highest)
+            batteries.append(highest)
             bank = bank[bank.index(highest)+1:]
-        answer.append(max(bank))
-        total += int("".join(map(str, answer)))
+        batteries.append(max(bank))
+        total += int("".join(map(str, batteries)))
         
     return total
 
 print(get_total_jolts(2))
 print(get_total_jolts(12))
+print(time() - s)
