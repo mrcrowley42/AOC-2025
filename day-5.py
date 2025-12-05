a8ad072201dab8b07dab461465a5a52644668e9a
mrcrowley42 <mattcrowley42@me.com>
2025-12-05 20:14:02 +1100

    Update day-5.py

--- day-5.py
+++ day-5.py
@@ -1,4 +1,4 @@
-with open("example_inputs/day-5.txt") as file:
+with open("inputs/day-5.txt") as file:
     ranges, ingredients = file.read().split("\n\n")
 
 ingredients = list(map(int, ingredients.splitlines()))
@@ -13,3 +13,14 @@ def is_fresh(score):
 
 print(sum([is_fresh(ingredient) for ingredient in ingredients]))
 
+sorted_ranges = sorted(ranges, key=lambda _range: _range[0])
+output = [sorted_ranges[0]]
+
+for lower, upper in sorted_ranges[1:]:
+    last = output[-1]
+    if lower <= last[1]:
+        output[-1] = (last[0], max(upper, last[1]))
+    else:
+        output.append((lower, upper))
+
+print(sum([(upper + 1 - lower) for lower, upper in output]))
