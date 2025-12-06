aa8cb1ba031059afc0a1adab06a1ae64e4dd1169
mrcrowley42 <mattcrowley42@me.com>
2025-12-06 16:58:23 +1100

    ew

--- day-6.py
+++ day-6.py
@@ -1,4 +1,21 @@
-with open("example_inputs/day-6.txt") as file:
-    data = file.read().splitlines()
+from math import prod
 
-print(data)
+
+with open("inputs/day-6.txt") as file:
+# with open("example_inputs/day-6.txt") as file:
+    data = file.read().splitlines()
+    
+vert = (list(zip(*[list(line) for line in data[:-1]])))
+new_nums = (",".join(["".join(x).strip() for x in vert]).split(",,"))
+new_equations = [list(map(int, e.split(","))) for e in new_nums]
+ops = data[-1].split()
+data = list(zip(*[line.split() for line in data[:-1]]))
+
+t1, t2 = 0, 0 
+for a, b, op in zip(data, new_equations, ops):
+    a = list(map(int, a))
+    t1 +=  sum(a) if op == "+" else prod(a)
+    t2 +=  sum(b) if op == "+" else prod(b)
+
+print(t1)
+print(t2)
