b3a6e992a12e4766d55dae545ea5d169b3d76e70
mrcrowley42 <mattcrowley42@me.com>
2025-12-08 18:15:41 +1100

    uh

--- day-8.py
+++ day-8.py
@@ -1,4 +1,19 @@
-with open("example_inputs/day-8.txt") as file:
-    data = file.read().splitlines()
+from itertools import combinations
+import networkx as nx
+import math
 
-print(data)
+with open("inputs/day-8.txt") as file:
+    data = [list(map(int, line.split(","))) for line in file.read().splitlines()]
+
+def distance(a, b):
+    diffs = [(p1 - p2) ** 2 for p1, p2 in zip(a, b)]
+    return sum(diffs) ** 0.5
+        
+distances = sorted([(distance(a, b), tuple(a), tuple(b)) for a, b in combinations(data, r=2)])
+
+g = nx.Graph()
+for v, p1, p2 in distances[:1000]:
+    g.add_edge(p1, p2)
+components = [len(c) for c in nx.connected_components(g)]
+components.sort(reverse=True)
+print(math.prod(components[:3]))
