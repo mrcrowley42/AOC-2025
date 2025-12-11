83b4b244f703c5fce0f7aa1bd5ddef09227d3c09
mrcrowley42 <mattcrowley42@me.com>
2025-12-11 18:17:19 +1100

    11

--- day-11.py
+++ day-11.py
@@ -1,4 +1,33 @@
-with open("example_inputs/day-11.txt") as file:
-    data = file.read().splitlines()
+with open("inputs/day-11.txt") as file:
+    data = [line.replace(":", "").split() for line in file.read().splitlines()]
 
-print(data)
+devices = {line[0]: line[1:] for line in data}
+
+# queue = [("you",)]
+# paths = 0
+# while queue:
+#     path = queue.pop()
+#     current = path[-1]
+#     if current == "out":
+#         paths += 1
+#         continue
+#     for child in devices[current]:
+#         queue.append((path) + (child,))
+
+# print(paths)
+
+queue = [("svr",)]
+paths = 0
+while queue:
+    path = queue.pop()
+    current = path[-1]
+    if current == "out":
+        paths += 1
+        if "fft" in path and "dac" in path:
+            print("done")
+            paths += 1
+        continue
+    for child in devices[current]:
+        queue.append((path) + (child,))
+
+print(paths)
